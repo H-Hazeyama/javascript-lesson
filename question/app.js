ca4d21b2de8e8e8cf078b71042eeabd44c92c198
H-Hazeyama <oqiri0707@gmail.com>
2025-09-01 18:09:32 +0900

    feat: Q5回答

--- question/app.js
+++ question/app.js
@@ -144,3 +144,15 @@ for (let i = 0; i < 100; i++) {
     numbers[i] = i;
     console.log(numbers[i]);
 }
+
+// Q5 for × if
+let mixed = [4, '2', 5, '8', '9', 0, 1];
+for (let i = 0; i < mixed.length; i++){
+    if ((typeof mixed[i]) === "number" && (mixed[i] % 2) === 0) {
+        console.log('even');
+    } else if (typeof mixed[i] === "number" && (mixed[i] % 2) !== 0) {
+        console.log('odd');
+    } else {
+        console.log('not number');
+    }
+}
